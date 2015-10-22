98071849e6aa9571c8193fc12f5db4a8b19a335f
hyeoksuk <hyeoksuk@gmail.com>
2015-10-22 17:36:04 +0900

    2015-10-21까지 푼 것 일괄 commit

---
+++ Algospot/src/khs/algo/easy/HELLOWORLD.java
@@ -0,0 +1,16 @@
+package khs.algo.easy;
+
+import java.util.Scanner;
+
+// https://algospot.com/judge/problem/read/HELLOWORLD
+public class HELLOWORLD {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int tc = sc.nextInt();
+		
+		while(tc-- > 0) {
+			String name = sc.next();
+			System.out.println("Hello, " + name + "!");
+		}
+	}
+}
