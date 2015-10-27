ed699a5c04a1789befa088050c147c10eb6b3d32
hyeoksuk <hyeoksuk@gmail.com>
2015-10-27 19:46:59 +0900

    다행히 통과

--- Algospot/src/khs/algo/easy/BRACKETS2.java
+++ Algospot/src/khs/algo/easy/BRACKETS2.java
@@ -1,57 +1,50 @@
 package khs.algo.easy;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 // https://algospot.com/judge/problem/read/BRACKETS2
 public class BRACKETS2 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int tc = sc.nextInt();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int tc = Integer.parseInt(br.readLine());
 		while(tc-- > 0) {
-			char[] inputs = sc.next().toCharArray();
+			char[] inputs = br.readLine().toCharArray();
+			int[] opens = new int[inputs.length];
 			
-			for(int i=0;i<inputs.length;i++) {
-				if(inputs[i] == ')') {
-					if(i-1 >= 0 && inputs[i-1] == '(') {
-						inputs = newInputs(inputs, i, i-1);
-						i -= 2;
-					} else {
-						break;
-					}
-				} else if(inputs[i] == '}') {
-					if(i-1 >= 0 && inputs[i-1] == '{') {
-						inputs = newInputs(inputs, i, i-1);
-						i -= 2;
-					} else {
-						break;
-					}
-				} else if(inputs[i] == ']') {
-					if(i-1 >= 0 && inputs[i-1] == '[') {
-						inputs = newInputs(inputs, i, i-1);
-						i -= 2;
-					} else {
-						break;
-					}
+			int opensIdx = 0;
+			boolean valid = true;
+			for(int i=0;i<inputs.length && valid;i++) {
+				switch(inputs[i]) {
+				case '(':
+					opens[opensIdx++] = 1;
+					break;
+				case '{':
+					opens[opensIdx++] = 2;
+					break;
+				case '[':
+					opens[opensIdx++] = 3;
+					break;
+				case ')':
+					opensIdx = (opensIdx - 1) < 0 ? 0 : opensIdx - 1;
+					valid = (opens[opensIdx] -= 1) == 0 ? true : false;
+					break;
+				case '}':
+					opensIdx = (opensIdx - 1) < 0 ? 0 : opensIdx - 1;
+					valid = (opens[opensIdx] -= 2) == 0 ? true : false;
+					break;
+				case ']':
+					opensIdx = (opensIdx - 1) < 0 ? 0 : opensIdx - 1;
+					valid = (opens[opensIdx] -= 3) == 0 ? true : false;
+					break;
 				}
 			}
-			
-			if(inputs.length == 0) {
+			if(opens[0] == 0) {
 				System.out.println("YES");
 			} else {
 				System.out.println("NO");
 			}
 		}
-	}
-	
-	private static char[] newInputs(char[] originInputs, int a, int b) {
-		char[] newInputs = new char[originInputs.length-2];
-		int ni = 0;
-		for(int i=0;i<originInputs.length;i++) {
-			if(i == a || i == b)
-				continue;
-			newInputs[ni++] = originInputs[i];
-		}
-		return newInputs;
 	}
 }
